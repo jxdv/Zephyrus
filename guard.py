3693ad7fc3982f823bdfa54a3c2b1deeb82bee3e
jxd1337 <virgoj@protonmail.com>
2023-08-31 21:04:41 +0200

    small work done only

--- guard.py
+++ guard.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
+import sys
+import platform
+import configparser
+
+from nixShield.monitor import Monitor
+
+
+def main():
+    if platform.system() != "Linux":
+        print("Non-UNIX system detected! Please run on a UNIX-like system.")
+        sys.exit(1)
+
+    if sys.version_info < (3, 6):
+        print("Python version 3.6 or newer required to run nixShield!")
+        sys.exit(1)
+
+    config = configparser.ConfigParser()
+    config.read("config/config.ini")
+    monitor = Monitor(config)
+    monitor.start()
+
+
 if __name__ == "__main__":
-    pass
+    main()
---
+++ nixShield/db.py
@@ -0,0 +1,6 @@
+import sqlite3
+
+
+class DatabaseHandler:
+    def __init__(self):
+        pass
