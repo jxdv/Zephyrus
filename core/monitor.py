f074c07e5df96bd8f5fba325a18e7dcf82b886d1
jxd1337 <virgoj@protonmail.com>
2023-10-19 19:14:59 +0200

    target filtering done

--- core/monitor.py
+++ core/monitor.py
@@ -1,34 +1,44 @@
 import sys
 import os
 
+from .target import Target
 from .log import ZephyrusLogger
 
 logger = ZephyrusLogger(__name__)
 
 
 class Monitor:
-    def __init__(self, target, hash_alg, verbose, threads):
+    def __init__(self, target, hash_alg, verbose, threads, ignored_prefixes, ignored_suffixes):
         self.target = target
         self.hash_alg = hash_alg
         self.verbose = verbose
         self.threads = threads
+        self.ignored_prefixes = ignored_prefixes
+        self.ignored_suffixes = ignored_suffixes
+        self.targets = []
+
+    def filter_check(self, file_path):
+        if self.ignored_prefixes:
+            return any(file_path.startswith(prefix) for prefix in self.ignored_prefixes)
+        if self.ignored_suffixes:
+            return any(file_path.endswith(suffix) for suffix in self.ignored_suffixes)
+        return False
 
     def _parse_target(self, target):
-        try:
-            with open(target, "r") as f:
-                targets = f.readlines()
-        except OSError as e:
-            logger.error(f"_parse_target caught: {e}")
-            sys.exit(1)
-        except Exception as e:
-            logger.error(f"_parse_target caught: {e}")
+        if not os.path.isdir(target):
+            logger.info(f"{target} is not a directory!")
             sys.exit(1)
 
-        if not targets:
-            logger.info("Target file is empty. There's nothing to monitor!")
+        for path, _, files in os.walk(target):
+            for name in files:
+                file_path = os.path.join(path, name)
+                if not self.filter_check(name):
+                    target = Target(file_path)
+                    self.targets.append(target)
+
+        if not self.targets:
+            logger.info("Target directory is empty!")
             sys.exit(1)
 
     def run(self):
         self._parse_target(self.target)
-
-        logger.info("Zephyrus initialized.")
--- zephyrus.py
+++ zephyrus.py
@@ -11,17 +11,17 @@ from core.utils import print_logo
 def main():
     args = parse_cli()
 
-    if args["version"]:
+    if args.version:
         print(f"Zephyrus version: {version}")
         sys.exit(0)
 
-    if not args["target"]:
+    if not args.target:
         print("Please specify a target file!")
         sys.exit(1)
 
     print_logo()
 
-    monitor = Monitor(args["target"], args["hash"], args["verbose"], args["threads"])
+    monitor = Monitor(args.target, args.hash, args.verbose, args.threads, args.ignored_prefixes, args.ignored_suffixes)
     monitor.run()
 
 
